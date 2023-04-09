b704b1d7aafa881eddf636f168a0189509210149
mananjain31 <mananjain31jan@gmail.com>
2023-04-09 11:34:55 +0530

    proxy fixed

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const api = axios.create({
+  baseURL: "https://backend-wittyhacks-production.up.railway.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",

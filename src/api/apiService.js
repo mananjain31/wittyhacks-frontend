b22b85df94560ac50cbf0898f2120a73527d2bb8
mananjain31 <mananjain31jan@gmail.com>
2023-04-09 12:15:24 +0530

    changes

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const api = axios.create({
   baseURL: "https://backend-wittyhacks-production.up.railway.app/",
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
